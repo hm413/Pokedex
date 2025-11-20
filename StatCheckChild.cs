3c8f48c2235da00788bec50b99619ca62a55a7ce
hm413 <mich6604@scdsb.on.ca>
2025-11-20 14:12:47 -0500

    Add files via upload

---
+++ StatCheckChild.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics; 
+
+namespace Pokedex
+{
+    public class ResourceChecker
+    {
+        public void CheckStats()
+        {
+        private static readonly List<string> itemlist = new List<string>() { "Ram", "CPU", "Storage" };
+        private static readonly PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% ProcessorTime", "_total");
+        private static readonly PerformanceCounter ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+        private static readonly PerformanceCounter storageCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "GB used.");
+
+        Dictionary<string, PerformanceCounter> assets = new Dictionary<string, PerformanceCounter>()
+        
+        public ResourceChecker()
+        {
+            try
+            {
+                cpuCounter.NextValue();
+                ramCounter.NextValue();
+                storageCounter.NextValue();
+            }
+        }
+      
+    }
+
+
+    
+
+
+}
+
+
